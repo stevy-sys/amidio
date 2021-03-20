e9d0e88a030289386ff54f73e294e2cf8da201bb
stevy-sys <62965624+stevy-sys@users.noreply.github.com>
2021-03-20 12:00:27 +0100

    initialisation

---
+++ resources/js/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                lorem dsfsjadhfkajhfkajha
+            </div>
+        )
+    }
+}
+
+export default App
+
+if (document.getElementById('root')) {
+    ReactDOM.render(<App/>,document.getElementById('root'));
+}
